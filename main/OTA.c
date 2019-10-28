140ad85aa077a2f1f260ab3f3530340f643c80fe
Adrian Kennard <a@k.gg>
2019-10-28 06:47:51 +0000

    Simpler

--- main/OTA.c
+++ main/OTA.c
@@ -29,33 +29,25 @@ app_main ()
    // Update partition table if necessary
    if (esp_partition_find_first (ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, "factory"))
    {                            // Factory exists, we want to change to new partition table.
-#define BLOCK SPI_FLASH_SEC_SIZE        // flash block size
-#define BLOCK_START (CONFIG_PARTITION_TABLE_OFFSET / BLOCK * BLOCK)
-#define PART_OFFSET (CONFIG_PARTITION_TABLE_OFFSET-BLOCK_START)
-#define	PART_SIZE (part_end-part_start)
-      revk_error (TAG, "Updating partition table (%d) block %d", PART_SIZE, BLOCK);
-      if (PART_OFFSET + PART_SIZE > BLOCK)
+      revk_error (TAG, "Updating partition table (%d) block %d", part_end - part_start, SPI_FLASH_SEC_SIZE);
+      if ((part_end - part_start) > SPI_FLASH_SEC_SIZE)
          revk_error (TAG, "Block size error");
-      uint8_t *mem = malloc (BLOCK);
+      uint8_t *mem = malloc (SPI_FLASH_SEC_SIZE);
       if (!mem)
-         revk_error (TAG, "Malloc fail: %d", BLOCK);
+         revk_error (TAG, "Malloc fail: %d", SPI_FLASH_SEC_SIZE);
       else
       {
          esp_err_t e;
-         if ((e = spi_flash_read (BLOCK_START, mem, BLOCK)))
-            revk_error (TAG, "Read fail:%s", esp_err_to_name (e));
+         memset (mem, 0, SPI_FLASH_SEC_SIZE);
+         memcpy (mem, part_start, part_end - part_start);
+         if ((e = spi_flash_erase_range (CONFIG_PARTITION_TABLE_OFFSET, SPI_FLASH_SEC_SIZE)))
+            revk_error (TAG, "Erase fail:%s", esp_err_to_name (e));
+         else if ((e = spi_flash_write (CONFIG_PARTITION_TABLE_OFFSET, mem, SPI_FLASH_SEC_SIZE)))
+            revk_error (TAG, "Write fail:%s", esp_err_to_name (e));
          else
          {
-            memcpy (mem + PART_OFFSET, part_start, PART_SIZE);
-            if ((e = spi_flash_erase_range (BLOCK_START, BLOCK)))
-               revk_error (TAG, "Erase fail:%s", esp_err_to_name (e));
-            else if ((e = spi_flash_write (BLOCK_START, mem, BLOCK)))
-               revk_error (TAG, "Write fail:%s", esp_err_to_name (e));
-            else
-            {
-               revk_info (TAG, "Partition table loaded");
-               revk_restart ("OTA complete", 5);
-            }
+            revk_info (TAG, "Partition table loaded");
+            revk_restart ("OTA complete", 5);
          }
       }
    }
