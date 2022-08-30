1243fb1cb318f13c21b01e91e4c66ccce6cb0a6f
Adrian Kennard <a@k.gg>
2022-08-30 09:41:41 +0100

    Update libraries to build with 4.4.2 and cut down - very simple exmaple now

--- Makefile
+++ Makefile
@@ -5,11 +5,15 @@
 
 PROJECT_NAME := OTA
 
-include $(IDF_PATH)/make/project.mk
+
+all:
+	@echo Make: $(PROJECT_NAME)$(SUFFIX).bin
+	@idf.py build
+	@cp build/$(PROJECT_NAME).bin $(PROJECT_NAME)$(SUFFIX).bin
+	@echo Done: $(PROJECT_NAME)$(SUFFIX).bin
 
 update:
 	git submodule update --init --recursive --remote
-	git commit -a -m "Library update"
 
 # Program the FTDI
 ftdi: ftdizap/ftdizap
@@ -17,3 +21,19 @@ ftdi: ftdizap/ftdizap
 
 ftdizap/ftdizap: ftdizap/ftdizap.c
 	make -C ftdizap
+
+set:    wroom solo pico
+
+pico:
+	components/ESP32-RevK/setbuildsuffix -S1-PICO-SSD1681
+	@make
+
+wroom:
+	components/ESP32-RevK/setbuildsuffix -S1-SSD1681
+	@make
+
+solo:   # This is Shellys and no display
+	components/ESP32-RevK/setbuildsuffix -S1-SOLO
+	@make
+
+
--- main/OTA.c
+++ main/OTA.c
@@ -1,30 +1,25 @@
 // OTA app, can be used to do an OTA from a small footprint. Also does partition update.
 // Warning, moving NVS will typically mess reset all of the settings
 // Copyright © 2019 Adrian Kennard, Andrews & Arnold Ltd. See LICENCE file for details. GPL 3.0
-static const char TAG[] = "OTA";
+static const char __attribute__((unused)) TAG[] = "OTA";
 
 #include "revk.h"
 #include <esp_spi_flash.h>
 
 static char *otaurl;
 
-const char *
-app_command (const char *tag, unsigned int len, const unsigned char *value)
+const char *app_callback(int client, const char *prefix, const char *target, const char *suffix, jo_t j)
 {
-   if (!strcmp (tag, "wifi") && *otaurl)
+   if (!strcmp(suffix, "wifi") && *otaurl)
    {
-      revk_ota (otaurl);
-      revk_setting ("otaurl", 0, NULL); // Clear the URL
+	revk_command("upgrade",NULL);
    }
-   if (!strcmp (tag, "connect"))
-      revk_info (TAG, "Running generic OTA system");
    return "";
 }
 
-void
-app_main ()
+void app_main()
 {
-   revk_init (&app_command);
-   revk_register ("otaurl", 0, 0, &otaurl, NULL, 0);
-   // Note, this used to do partition checks, but now done in revk library
+   revk_boot(&app_callback);
+   revk_register("otaurl", 0, 0, &otaurl, NULL, 0);
+   revk_start();
 }
